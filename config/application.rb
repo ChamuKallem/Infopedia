4289778a2100dd12cc6072bd5e2754c69d4f2a0e
Chamu Kallem <chamukallem@gmail.com>
2017-02-06 17:39:25 -0800

    Blocipedia:cp-2&3

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,14 @@ module Blocipedia
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    ActionMailer::Base.smtp_settings = {
+      :address  => 'smtp.gmail.com',
+      :domain   => 'mail.gmail.com',
+      :port     => 587,
+      :user_name  => "chamukallem@gmail.com",
+      :authentication => 'login',
+      :enable_starttls_auto => true
+    }
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   get 'welcome/about'
 
 
   root 'welcome#index'
-
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
