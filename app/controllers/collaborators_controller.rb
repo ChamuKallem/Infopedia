ee150ce1042f6cac04077b248a2aa2941c3e33ad
Chamu Kallem <chamukallem@gmail.com>
2017-02-19 20:42:29 -0800

    Blocipedia: blocipedia-cp-10-wip1

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -4,12 +4,6 @@ class CollaboratorsController < ApplicationController
       @wiki = Wiki.find(params[:wiki_id])
       id = "#{params[:collaborator]}"
       @user = User.where(id: id).first
-      puts @user.id
-      puts @user.email
-
-      puts "Wiki u are looking for"
-      puts @wiki.title
-
       @collaborator = Collaborator.new(wiki: @wiki, user: @user)
       # collaborator = current_user.collaborator.build(wiki: wiki, user: user)
       puts "setting collaborator"
@@ -20,5 +14,17 @@ class CollaboratorsController < ApplicationController
         flash[:alert] = "can't add a collaborator"
       end
       redirect_to @wiki
+  end
+  def destroy
+    @wiki = Wiki.find(params[:wiki_id])
+    id = "#{params[:collaborator]}"
+    @user = User.where(id: id).first
+    @collaborator = @wiki.collaborators.find(params[:id])
+    if @collaborator.destroy
+      flash[:notice] = "Added a collaborator"
+    else
+      flash[:alert] = "can't add a collaborator"
+    end
+    redirect_to @wiki
   end
 end
