1ea9fdb8dd2f2811cfad9765eaf171a142077159
Chamu Kallem <chamukallem@gmail.com>
2017-07-22 13:31:50 -0700

    adding css for sign-up and redirecting to wikis on sign-up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
   def after_sign_up_path_for(resource)
-    '/charges/new'
+    '/wikis'
+    # '/charges/new'
   end
 end
